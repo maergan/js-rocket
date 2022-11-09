7fe6febaea33803bb6342c47e17a4bdfde89592a
maergan <117617134+maergan@users.noreply.github.com>
2022-11-08 22:24:19 -0600

    Boolean updates

--- js/rocket.js
+++ js/rocket.js
@@ -37,7 +37,7 @@ var changeState = function (state) {
             var randomNumber = Math.round(Math.random()*10);
             console.log('randomNumber:', randomNumber)
             //success
-            if (randomNumber > 9) {
+            if (randomNumber > 5) {
                 changeState(4);
             }
             //failure
