794d88d7453d956a0f8910dd3a98e07d03e0eceb
maergan <117617134+maergan@users.noreply.github.com>
2022-11-11 11:21:12 -0600

    Redirected from index

--- js/rocket.js
+++ js/rocket.js
@@ -1,49 +1,47 @@
 var timer = null;
 var countdownNumber = 10;
 
-setTimeout(function (){
-    location.href='rocket.html';
-}, 2000);
 
-var changeState = function (state) {
-    document.body.className = 'body-state'+state; 
+
+function changeState(state) {
+    document.body.className = 'body-state' + state;
     clearInterval(timer);
     countdownNumber = 10;
     document.getElementById('countdown').innerHTML = countdownNumber;
 
 
-    if (state == 2){
+    if (state == 2) {
         timer = setInterval(function () {
             countdownNumber = countdownNumber - 1;
             document.getElementById('countdown').innerHTML = countdownNumber;
-            
-            if (countdownNumber > 3 && countdownNumber <= 5) {
-				// be nervous
-				document.getElementById('nervous').className = 'nervous show';
-			} else{
-				document.getElementById('nervous').className = 'nervous';
-			}
 
-			if (countdownNumber > 6 && countdownNumber <= 8) {
-				// to the moon
-				document.getElementById('moon').className = 'moon show';
-			} else {
-				document.getElementById('moon').className = 'moon';
-			}
-        
-        if (countdownNumber <= 0){
-            changeState(3);
-        }
+            if (countdownNumber > 3 && countdownNumber <= 5) {
+                // be nervous
+                document.getElementById('nervous').className = 'nervous show';
+            } else {
+                document.getElementById('nervous').className = 'nervous';
+            }
+
+            if (countdownNumber > 6 && countdownNumber <= 8) {
+                // to the moon
+                document.getElementById('moon').className = 'moon show';
+            } else {
+                document.getElementById('moon').className = 'moon';
+            }
+
+            if (countdownNumber <= 0) {
+                changeState(3);
+            }
         }, 500);
-    } else if (state == 3){
-        var success = setTimeout(function ()
-        {
-            var randomNumber = Math.round(Math.random()*10);
-            console.log('randomNumber:', randomNumber)
+    } else if (state == 3) {
+        var success = setTimeout(function () {
+            var randomNumber = Math.round(Math.random() * 10);
+            console.log('randomNumber:', randomNumber);
             //success
             if (randomNumber > 1) {
                 changeState(4);
             }
+
             //failure
             else {
                 changeState(5); //oh no!
